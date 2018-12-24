9de5c7b2a53cc2d04d8941311356fec089cb96e8
lemonart <lemonart@310-12.local>
2018-12-24 13:29:03 +0800

    first commit

---
+++ accountbook/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  accountbook
+//
+//  Created by lemonart on 2018/12/10.
+//  Copyright © 2018 Lemon. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
